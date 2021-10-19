f0a95c69b739839d0cb6fdef2329073b9c67fe70
AkshayAkkalagari <86343905+AkshayAkkalagari@users.noreply.github.com>
2021-10-19 12:29:34 +0530

    Create operations.js

---
+++ operations.js
@@ -0,0 +1,22 @@
+// MongoDB -> Powerfull
+
+// logical operators
+
+//$sinc => increment, -1, -2, -3
+//$min => minimum
+//$max => maximum
+//$set => used to set a data
+//book.title = "xyz"
+
+//$unset => removing a property from an object
+
+//Arryay operators
+//$push
+//name = ["Akshay", "xxyyzz"]
+//$pop
+//used to extract /remove/delete the last element
+//$pull
+//pull: {
+//          name: "xyzzz"
+//      }
+//$addtoset => dosen't allow duplicates.
