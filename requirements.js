85015a23758f757a9f66fd110e93a0388ce2bf81
AkshayAkkalagari <86343905+AkshayAkkalagari@users.noreply.github.com>
2021-10-19 12:30:06 +0530

    Create requirements.js

---
+++ requirements.js
@@ -0,0 +1,58 @@
+//Requirement for our project
+
+//We are a book management company 
+
+//BOOKS 
+//ISBN, title, pub date, language, num page, author[], category[]
+
+//AUTHORS
+//id, name, books[]
+
+//PUBLICATIONS
+//id, name, books[]
+
+//We have to design and code an API over this . 
+
+//1. BOOKS 
+//We need an API :- 
+//To get all the books - done
+//To get specific book - done
+//To get a list of books based on category - done
+//To get a list of books based on languages - done
+
+//2. AUTHORS
+//We need an API :-
+//To get all the authors - done
+//To get a specific author based on id - 
+//To get a list of authors based on books - done
+
+//3. PUBLICATIONS
+//We need an API :-
+//To get all the publications - done
+//To get a specific publication -
+//To get a list of publications based on a book -   
+
+///////////////////  post  ////////////////
+//add new book - done
+//add new publications - done
+// add new author - 
+// chech the data if available update it or add -
+
+//////////////////   put   ////////////////
+
+//update book details if author is changed.
+
+/////////////////    delete   ////////////////
+
+//1. delete book
+//2. delete author from book
+//3. delete author from book and related book from author
+
+
+// Schema - blueprint of how data has to be constructed
+// MongoDB is Schemaless
+//Mongoose has Schema
+// mongoose - validation, relationship with other data.
+//model -> document model of MongoDB
+
+//Schema -> Model -> use them.
